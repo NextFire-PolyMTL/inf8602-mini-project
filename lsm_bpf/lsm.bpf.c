4b409610bd3e8a4482b20a7939a8f302c38aca42
NextFire <git@yuru.moe>
2024-03-26 13:44:33 -0400

    lsm_bpf: printk

--- lsm_bpf/lsm.bpf.c
+++ lsm_bpf/lsm.bpf.c
@@ -5,11 +5,12 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+#include "printk.bpf.h"
 
 #define EPERM 1
 #define S_ISUID 04000
 
-char _license[] SEC("license") = "GPL";
+char LICENSE[] SEC("license") = "GPL";
 
 /**
  * LSM_HOOK(int, 0, inode_copy_up, struct dentry *src, struct cred **new)
@@ -20,15 +21,20 @@ char _license[] SEC("license") = "GPL";
 SEC("lsm/inode_copy_up")
 int BPF_PROG(restrict_inode_copy_up, struct dentry *src, struct cred **new, int ret)
 {
-    if (ret)
+    if (ret != 0)
+    {
         return ret;
+    }
 
     struct inode *inode = src->d_inode;
 
     // If the file has the setuid bit set and the owner is root,
     // deny the copy-up operation
-    if (inode->i_mode & S_ISUID && inode->i_uid.val == 0)
+    if ((inode->i_mode & S_ISUID) && (inode->i_uid.val == 0))
+    {
+        bpf_printk("lsm_bpf: inode_copy_up: deny copy-up of setuid root file \"%s\"\n", src->d_name.name);
         return -EPERM;
+    }
 
     return 0;
 }
--- lsm_bpf/lsm.user.c
+++ lsm_bpf/lsm.user.c
@@ -2,52 +2,91 @@
  * User-space program that loads and attaches the BPF program to the LSM hook.
  *
  * Adapted from:
- * https://blog.cloudflare.com/live-patch-security-vulnerabilities-with-ebpf-lsm (deny_unshare.c)
+ * https://github.com/leodido/demo-cloud-native-ebpf-day/blob/da96b542c258f69c95bd27c2159890a6f493b208/src/restrict_connect.c
+ * https://github.com/leodido/demo-cloud-native-ebpf-day/blob/da96b542c258f69c95bd27c2159890a6f493b208/src/utils.c
  */
 
-#include <bpf/libbpf.h>
+#include <stdio.h>
+// #include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <signal.h>
+// #include <errno.h>
+// #include <sys/resource.h>
+// #include <bpf/libbpf.h>
 #include "lsm_bpf.skel.h"
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
-    return vfprintf(stderr, format, args);
+    return vfprintf(stdout, format, args);
 }
 
-int main(int argc, char *argv[])
+static volatile sig_atomic_t stop;
+
+int bpf_trace_pipe(int out)
+{
+    // todo > find mount -> use mnt/trace_pipe (making strong assumptions atm)
+
+    int inp = STDERR_FILENO;
+    inp = open("/sys/kernel/debug/tracing/trace_pipe", O_RDONLY);
+    if (inp < 0)
+    {
+        return inp;
+    }
+
+    while (!stop)
+    {
+        static char buf[4096];
+        ssize_t ret;
+
+        ret = read(inp, buf, sizeof(buf));
+        if (ret > 0 && write(out, buf, ret) == ret)
+        {
+            continue;
+        }
+    }
+
+    close(inp);
+    return 0;
+}
+
+int main(int argc, char **argv)
 {
     struct lsm_bpf_c *skel;
-    int err = 0;
+    int err;
 
-    libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+    /* Set up libbpf errors and debug info callback */
     libbpf_set_print(libbpf_print_fn);
 
-    // Loads and verifies the BPF program
+    /* Bump RLIMIT_MEMLOCK to allow BPF sub-system to do anything */
+    // bump_memlock_rlimit();
+
+    // if (signal(SIGINT, sig_int) == SIG_ERR || signal(SIGTERM, sig_int) == SIG_ERR)
+    // {
+    //     fprintf(stderr, "Can't handle Ctrl-C: %s\n", strerror(errno));
+    //     goto cleanup;
+    // }
+
+    /* Open load and verify BPF application */
     skel = lsm_bpf_c__open_and_load();
     if (!skel)
     {
-        fprintf(stderr, "failed to load and verify BPF skeleton\n");
-        goto cleanup;
+        fprintf(stderr, "Failed to open BPF skeleton\n");
+        return 1;
     }
+    fprintf(stdout, "BPF skeleton ok\n");
 
-    // Attaches the loaded BPF program to the LSM hook
+    /* Attach tracepoint handler */
     err = lsm_bpf_c__attach(skel);
     if (err)
     {
-        fprintf(stderr, "failed to attach BPF skeleton\n");
+        fprintf(stderr, "Failed to attach BPF skeleton\n");
         goto cleanup;
     }
 
-    printf("LSM loaded! ctrl+c to exit.\n");
-
-    // The BPF link is not pinned, therefore exiting will remove program
-    for (;;)
-    {
-        fprintf(stderr, ".");
-        sleep(1);
-    }
+    err = bpf_trace_pipe(STDERR_FILENO);
 
 cleanup:
     lsm_bpf_c__destroy(skel);
-    return err;
+    return -err;
 }
